ee114701be64ce669d8c59ea974fdd584be262ae
bjornkai <bjornkairoland@gmail.com>
2019-03-04 20:29:05 -0500

    connected heroku

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const session = require('express-session');
 const passportSetup = require('./config/passport/passport-setup');
 
 mongoose
-  .connect('mongodb://localhost/project-2', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
